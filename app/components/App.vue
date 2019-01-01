90f6c875c98cc33fbe870f594f199db4fcaa90be
Hubert Stärk <hubert.staerk@gmail.com>
2019-01-02 00:02:02 +0100

    Initial commit

---
+++ app/components/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <Page>
+        <ActionBar title="Test!"/>
+        <GridLayout columns="*" rows="*">
+            <Label class="message" :text="msg" col="0" row="0"/>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+  // e528c09ab86a80f586e637b49c827957a734fb3f
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
